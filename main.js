cada5a639387bed492383251c4e1ec8a246a4364
xusniya123 <xusniya12345678@gmail.com>
2024-04-26 16:58:16 +0500

    Lesson 5 moth (7)

---
+++ main.js
@@ -0,0 +1,23 @@
+// const title = document.createElement ("h1");
+// const text = document.createTextNode("Hello Mars");
+// title.appendChild(text);
+// console.log(title);
+// const mainCode = document.querySelector("body");
+// mainCode.appendChild(title);
+
+
+// const changeTitle = () => {
+//     const title = document.querySelector("h1")
+//     title.innerText = "Hello Mars"
+//     title.style.color = "red"
+// }
+// let test = document.getElementsByClassName(test);
+// test [0].innerHtml = "Hello Mars"
+// test.style.backgroundColor = "red"
+
+// const numbers = [1,2,3,4,5];
+// const doubled = numbers.map(function(item) {
+// return item *2 ;
+// })
+// console.log(doubled);
+
